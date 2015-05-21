973b2ffcc2abf1767dfdfbfc56ab8c565e280f20
Robin <robin@roseglasses.org>
2015-05-21 14:28:21 -0700

    add matrix intersection/collision

--- src/tetris/core.clj
+++ src/tetris/core.clj
@@ -9,3 +9,14 @@
 
 (def board (atom (empty-board 10 10)))
 
+(defn submat [mat [row col] [width height]]
+  "extract region from matrix"
+  (map #(subvec % col (+ col width))
+       (subvec mat row (+ row height))))
+
+(defn intersects-v [v1 v2]
+  (map #(and %1 %2) v1 v2))
+
+(defn intersects-m [m1 m2]
+  (not-every? nil? (mapcat intersects-v m1 m2)))
+
